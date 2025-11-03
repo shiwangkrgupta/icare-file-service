b14a612770763af0ca0ed0864279d481f6feab8f
shiwangkrgupta <swdcaptain@gmail.com>
2025-11-03 18:49:32 +0530

    introduced .env file and dotenv extension for loading configs directly for .env files

--- src/main/java/com/icare/file_service/FileServiceApplication.java
+++ src/main/java/com/icare/file_service/FileServiceApplication.java
@@ -1,5 +1,6 @@
 package com.icare.file_service;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FileServiceApplication {
 
 	public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+
+        System.setProperty("file.upload-dir", dotenv.get("UPLOAD_DIR"));
+        System.setProperty("file.base-url", dotenv.get("FILE_BASE_URL"));
+        System.setProperty("server.port", dotenv.get("SERVER_PORT"));
+        System.setProperty("server.address", dotenv.get("SERVER_ADDRESS"));
+
 		SpringApplication.run(FileServiceApplication.class, args);
 	}
 
--- src/test/java/com/icare/file_service/FileServiceApplicationTests.java
+++ src/test/java/com/icare/file_service/FileServiceApplicationTests.java
@@ -6,6 +6,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FileServiceApplicationTests {
 
+    static {
+        io.github.cdimascio.dotenv.Dotenv dotenv = io.github.cdimascio.dotenv.Dotenv.load();
+        System.setProperty("file.upload-dir", dotenv.get("UPLOAD_DIR"));
+        System.setProperty("file.base-url", dotenv.get("FILE_BASE_URL"));
+        System.setProperty("server.port", dotenv.get("SERVER_PORT"));
+        System.setProperty("server.address", dotenv.get("SERVER_ADDRESS"));
+    }
+
 	@Test
 	void contextLoads() {
 	}
